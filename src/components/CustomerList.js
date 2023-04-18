4d6e41968a313e8ddf72ef8772072842603a0542
The Swiss Bell <TheSwissBell@outlook.com>
2023-04-18 16:47:01 +0300

    Customers list working

---
+++ src/components/CustomerList.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+
+import { API_URL } from '../constants'
+
+
+export default function Customerlist() {
+    const [customers, setCustomers] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [snackbarMsg, setSnackbarMsg] = '';
+
+    useEffect(() => fetchCustomers(), [])
+
+    const fetchCustomers = () => {
+        fetch(API_URL + 'customers')
+            .then(response => response.json())
+            .then(data => setCustomers(data.content))
+    }
+
+    const gridRef = useRef();
+
+    
+
+
+    // Grid
+    const [columnDefs] = useState([  // We don't to update it so no need for setColumnDefs
+        { field: 'firstname' },
+        { field: 'lastname' },
+        { field: 'streetaddress', width: 170 },
+        { field: 'postcode', width: 100 },
+        { field: 'city' },
+        { field: 'email', width: 180 }
+
+    ]);
+    const defaultColDef = useMemo(() => ({
+        sortable: true,
+        filter: true,
+        width: 154,
+        cellClass: 'ag-left-aligned-cell'
+    }))
+
+    return (
+        <div>
+            <div className="ag-theme-material" style={{ height: 600, width: 1000, margin: 'auto' }}>
+                <AgGridReact
+                    ref={gridRef}
+                    onGridReady={params => gridRef.current = params.api}
+                    rowSelection="single"
+                    rowData={customers}
+                    columnDefs={columnDefs}
+                    defaultColDef={defaultColDef}
+                    animateRows={true}
+                >
+                </AgGridReact>
+            </div>
+            <Snackbar
+                open={open}
+                message={snackbarMsg}
+                autoHideDuration={3000}
+                onClose={() => setOpen(false)}
+            />
+        </div>
+    );
+}
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const API_URL = 'https://traineeapp.azurewebsites.net/api';
+export const API_URL = 'https://traineeapp.azurewebsites.net/api/';
